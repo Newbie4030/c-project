e3f7610e4cedc9c14586a71d2e489b48eaa66790
RrahulSurre <86652324+Newbie4030@users.noreply.github.com>
2022-02-26 17:35:55 +0530

    dsc
    dsd

--- big3.c
+++ big3.c
@@ -2,7 +2,7 @@
 void biggest3()
 {  
     int num1, num2, num3;
-    printf(" Enter the number1 = ");
+    printf(" Enter the number1 = ");   
     scanf("%d", &num1);
     printf("\n Enter the number2 = ");
     scanf("%d", &num2);
